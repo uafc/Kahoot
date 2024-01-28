ca9774ddf2130ddfa2fd913225a4b56cae57f922
abinandn1 <77420355+abinandn1@users.noreply.github.com>
2024-01-27 17:31:28 -0700

    added web scraping boilerplate code

---
+++ webscraping_boilerplate.py
@@ -0,0 +1,23 @@
+from bs4 import BeautifulSoup
+import requests
+
+def scrape_questions_and_answers():
+    dict = {}  # Using a dictionary for questions and answers
+    url = ""
+    response = requests.get(url)
+    
+    #check if scraping is allowed 
+    if response.status_code != 200:
+        print(f"Failed to retrieve the webpage. Status code: {response.status_code}")
+        return
+    
+    soup = BeautifulSoup(response.text, "html.parser")
+    
+    #write your webscraping logic/code below!
+
+    return dict
+
+def main():
+    scraped_data = scrape_questions_and_answers()
+
+main()
